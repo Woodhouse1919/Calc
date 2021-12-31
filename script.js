1b6d1ddec16859eb3a6cd21cc2144cf705da6727
Woodhouse <pariswoodhouse@hotmail.com>
2021-12-31 12:51:52 -0800

    basic operator function created

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 const result = document.querySelector('.result').textContent;
-const number = document.querySelectorAll('.number')
+const number = document.querySelectorAll('.number');
 
-console.log(result)
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+function operate(operator, num1, num2) {
+  if (operator === add) {
+    return add(num1, num2);
+  } else if (operator === subtract) {
+    return subtract(num1, num2);
+  } else if (operator === multiply) {
+    return multiply(num1, num2);
+  } else if (operator === divide) {
+    return divide(num1, num2);
+  }
+}
