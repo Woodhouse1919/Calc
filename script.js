8a8bdc6bce729c431afd2cd10aa05ab3cc0e5af1
Woodhouse <pariswoodhouse@hotmail.com>
2022-01-09 12:43:59 -0800

    began working on operator func & updating screen

--- script.js
+++ script.js
@@ -1,5 +1,50 @@
-const result = document.querySelector('.result').textContent;
+let display = document.querySelector('.result');
+let storedDisplay = document.querySelector('.stored-equation');
 const number = document.querySelectorAll('.number');
+const operator = document.querySelectorAll('.operator');
+const equals = document.querySelector('#equals');
+let equation = [];
+let storedEquation = [];
+let chosenOperator = '';
+
+// stores numbers that user clicks into the equation variable and displays it in the calc screen
+number.forEach((num) => num.addEventListener('click', (e) => screenUpdate(e)));
+
+function screenUpdate(e) {
+  equation += e.target.textContent;
+  display.textContent = equation;
+}
+
+operator.forEach((op) => {
+  op.addEventListener('click', (e) => {
+    if (e.target.textContent === '+') {
+      if (equation.length > 0) {
+        chosenOperator = add;
+        storedEquation = equation;
+        equation = [];
+        storedDisplay.textContent = `${storedEquation} +`;
+      }
+    }
+  });
+});
+
+equals.addEventListener('click', () => {
+  result = operate(chosenOperator, Number(storedEquation), Number(equation));
+  display.textContent = result;
+  storedEquation = result;
+});
+
+function operate(operator, num1, num2) {
+  if (operator === add) {
+    return add(num1, num2);
+  } else if (operator === subtract) {
+    return subtract(num1, num2);
+  } else if (operator === multiply) {
+    return multiply(num1, num2);
+  } else if (operator === divide) {
+    return divide(num1, num2);
+  }
+}
 
 const add = (a, b) => {
   return a + b;
@@ -16,15 +61,3 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
   return a / b;
 };
-
-function operate(operator, num1, num2) {
-  if (operator === add) {
-    return add(num1, num2);
-  } else if (operator === subtract) {
-    return subtract(num1, num2);
-  } else if (operator === multiply) {
-    return multiply(num1, num2);
-  } else if (operator === divide) {
-    return divide(num1, num2);
-  }
-}
